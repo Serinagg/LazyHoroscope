87b624761c8aab1ff9266ab63777be4380c86a17
serinagg <sgarcia14@mail.csuchico.edu>
2018-09-19 17:13:51 -0700

    first name and last name input

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Serina Garcia
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,15 @@ using namespace std;
 int main()
 {
   //declare variables
+  string firstName;
+  string lastName;
 
   //get user input
+  cout<<"What is your first name?";
+  cin>>firstName;
+
+  cout<<"what is your last name?";
+  cin>>lastName;
 
   //tell fortune
 
