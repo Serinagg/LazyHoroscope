8c0614ce50b797b02de73a704a8afde9e9189727
serinagg <sgarcia14@mail.csuchico.edu>
2018-09-24 20:01:55 -0700

    lazy Horoscope pt2 complete

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
   string firstName;
   string lastName;
+  int leng;
 
   //get user input
   cout<<"What is your first name?\n";
@@ -16,11 +17,29 @@ int main()
 
   cout<<"what is your last name?\n";
   cin>>lastName;
+  leng = lastName.length() - 1;
   
   cout<<"Welcome, "<<firstName[0]<<"."<<lastName[0]<<"., here is your fortune..."<<endl;
   
   //tell fortune
   cout<<"Your Lucky Number is "<<firstName.length()<<"!"<<endl;
+
+  if (firstName[0] == 'a' || firstName[0] == 'A'|| firstName[0]== 'i'|| firstName[0] =='I'|| firstName[0] == 'o' || firstName[0] == 'O' || firstName[0]=='U' || firstName[0] =='u'){
+          cout<<"You are destined to be famous!\n";
+
+  } else {
+          cout<<"you should keep a low profile today.\n";
+
+  }
+
+if ( lastName[leng] == 'a' || lastName[leng] == 'A' || lastName[leng] == 'e' || lastName[leng] == 'E' || lastName[leng] == 'i' || lastName[leng] == 'I' || lastName[leng] == 'o' || lastName[leng] == 'O' || lastName[leng] == 'u' || lastName[leng] == 'U'){
+        cout<<"you have already met your true love!\n";
+
+} else {
+        cout<<"You haven't met your true love yet.\n";
+
+}
+cout<<"Have a good day!\n";
 
   return 0;
 }
